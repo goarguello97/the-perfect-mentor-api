822c0330201feeca0a791023f2a138d1d63a9ab9
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-11-26 17:06:13 -0300

    feat(firebase): added firebase admin sdk
    - .env with enviroment variables created
    - added config with serviceAccount of firebase

---
+++ src/firebase/firebase-admin.ts
@@ -0,0 +1,10 @@
+import * as admin from "firebase-admin";
+
+import serviceAccount from "../../config/serviceAccountKey.json";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+
+export default admin;
