b3e01eaf9eb36a99770401bc9a6e23d7a227ad33
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-11-26 17:38:41 -0300

    feat(firebase): error handling added
    - verified that the url is correctly from enviroments variables

--- src/firebase/firebase-admin.ts
+++ src/firebase/firebase-admin.ts
@@ -2,9 +2,16 @@ import * as admin from "firebase-admin";
 
 import serviceAccount from "../../config/serviceAccountKey.json";
 
+const databaseURL = process.env.FIREBASE_DATABASE_URL;
+
+if (!databaseURL)
+  throw new Error(
+    "La variable de entorno FIREBASE_DATABASE_URL no está definida."
+  );
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  databaseURL,
 });
 
 export default admin;
