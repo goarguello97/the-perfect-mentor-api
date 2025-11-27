f60fcc61d563450ec363a9708d8af58a199fb474
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-11-27 11:49:14 -0300

    feat(roles): added automatic creation roles function
    - added ejecution in server index

---
+++ src/config/createRoles.ts
@@ -0,0 +1,17 @@
+import Role from "../models/Role";
+
+const createRoles = async () => {
+  try {
+    const count = await Role.countDocuments();
+    if (count > 0) return;
+    await Promise.all([
+      new Role({ role: "MENTEE" }).save(),
+      new Role({ role: "MENTOR" }).save(),
+      new Role({ role: "ADMIN" }).save(),
+    ]);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default createRoles;
