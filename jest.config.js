67323dba755257ec1dc47c45afc85afc82c6548e
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-11-27 12:17:08 -0300

    feat(test): started test suites
    - added jest and supertest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports={
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  testMatch: ["**/__test__/**/*.test.ts"],
+  testTimeout: 100000,
+};
