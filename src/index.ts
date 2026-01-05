8b3d52d1912366a2fe88156d71c80c7e3349c0bd
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2026-01-05 18:05:47 -0300

    feat: added firebase functions
    - implemented Cors middleware
    - refactored getUserById and AddUser in UserService and UserController

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
-import * as dotenv from "dotenv";
+import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import createRoles from "./config/createRoles";
 import connectDb from "./db";
-import admin from "./firebase/firebase-admin";
 import router from "./routes/index.routes";
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
 
 const app = express();
-console.log("Firebase conectado a", admin.app().name);
-connectDb();
-createRoles();
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
+
+connectDb();
+createRoles();
 
 app.get("/", (_, res) => {
   res.send("Hello World");
