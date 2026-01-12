5eb4a4ca44a188e688ccdc2f8b6820102e3ff9c1
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2026-01-12 11:32:49 -0300

    feat: modified match model

--- src/models/Match.ts
+++ src/models/Match.ts
@@ -1,18 +1,29 @@
 import { model, Schema, Types } from "mongoose";
 
+export enum MatchStatus {
+  PENDING = "pending",
+  ACCEPTED = "accepted",
+  REJECTED = "rejected",
+}
 interface IMatch {
-  user: Types.ObjectId;
-  userMatch: Types.ObjectId;
-  accepted: boolean;
+  senderId: Types.ObjectId;
+  receiverId: Types.ObjectId;
+  status: MatchStatus;
 }
 
 const MatchSchema = new Schema<IMatch>(
   {
-    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    userMatch: { type: Schema.Types.ObjectId, ref: "User", requided: true },
-    accepted: { type: Boolean, default: false },
+    senderId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    receiverId: { type: Schema.Types.ObjectId, ref: "User", requided: true },
+    status: {
+      type: String,
+      enum: Object.values(MatchStatus),
+      default: MatchStatus.PENDING,
+    },
   },
   { timestamps: true, versionKey: false }
 );
+
+MatchSchema.index({ senderId: 1, receiverId: 1 }, { unique: true });
 
 export default model("Match", MatchSchema);
