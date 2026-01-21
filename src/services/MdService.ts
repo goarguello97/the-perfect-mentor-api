f00de90f0fe1abd1f4c103d3122172efef0ef6dd
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2026-01-21 17:42:56 -0300

    feat: implemented getUserMessages service
    - implemented query of all a user's chats

--- src/services/MdService.ts
+++ src/services/MdService.ts
@@ -1,5 +1,6 @@
 import Md from "@models/Md";
 import User from "@models/User";
+import mongoose from "mongoose";
 import { socketService } from "./SocketService";
 
 class MdService {
@@ -34,9 +35,95 @@ class MdService {
     }
   }
 
+  static async getUserMessages(data: {userId: string}) {
+    const { userId } = data
+    const userObjectId = new mongoose.Types.ObjectId(userId);
+    try {
+      if (!userId) throw new Error("Id usuario inexistente")
+      
+      const user = await User.findById(userId)
+
+      if (!user) throw new Error("Usuario inexistente")
+      
+      const messages = await Md.aggregate([
+        {
+          $match: {
+            $or: [
+              { receiverId: userObjectId },
+              { senderId: userObjectId }
+            ]
+          }
+        },          
+        { $sort: { createdAt: -1 } },
+        {
+          $addFields: {
+            chatPartner: {
+            $cond: [
+              { $eq: ["$senderId", userObjectId] },
+              "$receiverId",
+              "$senderId"
+            ]
+          }
+          }
+        },
+        {
+          $group: {
+            _id: "$chatPartner",
+            lastMessage: { $first: "$$ROOT" },
+            unreadCount: {
+              $sum: {
+                $cond: [
+                  {
+                    $and: [
+                      { $eq: ["$receiverId", userObjectId] },
+                      { $eq: ["$read", false] }
+                    ]
+                  },
+                  1,
+                  0
+                ]
+              }
+            }
+          }
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "_id",
+            foreignField: "_id",
+            as: "contactInfo"
+          }
+        },
+        {
+          $unwind: "$contactInfo"
+        },
+        {
+          $sort: {"lastMessage.createdAt": -1}
+        },
+        {
+          $project: {
+            _id: 0,
+            contact: {
+              _id: "$contactInfo._id",
+              fullname: "$contactInfo.fullname",
+              avatar: "$contactInfo.avatar"
+            },
+            lastMessage: 1,
+            unreadCount: 1
+          }
+        }
+      ])
+      
+      return {error:false, data:messages}   
+    } catch (error) {      
+      return { error: true, data: error };
+    }
+  }
+
   static async getMessages(data: { receiverId: string; senderId: string }) {
     try {
       const { receiverId, senderId } = data;
+
       const senderUser = await User.findById(senderId);
 
       if (!senderUser) throw new Error("Usuario emisor inexistente");
@@ -44,6 +131,11 @@ class MdService {
       const receiverUser = await User.findById(receiverId);
 
       if (!receiverUser) throw new Error("Usuario receptor inexistente");
+
+      await Md.updateMany(
+      { receiverId: senderId, senderId: receiverId, read: false },
+      { $set: { read: true } }
+      );
 
       const messages = await Md.find({
         $or: [
